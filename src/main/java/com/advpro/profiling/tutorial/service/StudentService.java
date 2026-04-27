e492c25d59ace8b7cf0adf92ce7f3322e2ad1eb8
Rehema Zurafa Saputra <rehemazurafasaputra@gmail.com>
2026-04-27 15:05:03 +0700

    [Refactoring] - Optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -41,12 +41,9 @@ public class StudentService {
     }
 
     public String joinStudentNames() {
-        List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
-        }
-        return result.substring(0, result.length() - 2);
+        List<String> names = studentRepository.findAllNamesOnly();
+
+        return String.join(", ", names);
     }
 }
 
